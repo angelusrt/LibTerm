78775854a850632ad1cd63e186f0ef44520e6c41
Angelus Rocha <angelusrt@gmail.com>
2024-07-20 17:49:35 -0300

    adicionando biblioteca de vectors

---
+++ lib/vectors.c
@@ -0,0 +1,159 @@
+#ifndef vectors_c
+#define vectors_c
+#pragma once
+
+#include "vectors.h"
+#include "strings.c"
+
+size_t vectors_types_size(vectors_type type) {
+	switch (type) {
+		case vectors_string_type: 
+			return sizeof(string);
+		case vectors_vector_type: 
+			return sizeof(vector);
+		case vectors_size_type: 
+			return sizeof(size_t);
+		case vectors_string_virtual_type:
+			return sizeof(string_virtual);
+	}
+
+	return sizeof(size_t);
+}
+
+void vectors_types_insert(vectors_type type, void *data, void *item, size_t index) {
+	switch (type) {
+		case vectors_string_type: 
+			((string *)data)[index] = *(string *)item; 
+			return;
+		case vectors_vector_type: 
+			((vector *)data)[index] = *(vector *)item; 
+			return;
+		case vectors_size_type: 
+			((size_t *)data)[index] = (size_t)item; 
+			return;
+		case vectors_string_virtual_type:
+			((string_virtual *)data)[index] = *(string_virtual *)item;
+			return;
+	}
+
+	((size_t *)data)[index] = (size_t)item; 
+	return;
+}
+
+void vectors_types_cleanup(vectors_type type, void *data, size_t index) {
+	switch (type) {
+		case vectors_string_type: 
+			strings_free(&((string *)data)[index]); 
+			return;
+		case vectors_vector_type: 
+			vectors_free(&((vector *)data)[index]); 
+			return;
+		case vectors_size_type: 
+		case vectors_string_virtual_type:
+			return;
+	}
+
+	return;
+}
+
+vector vectors_init(vectors_type type) {
+	size_t type_size = vectors_types_size(type);
+	void *data = malloc(vectors_min*type_size);
+	errors_panic("vectors_init (data)", data == NULL);
+
+	return (vector){.size=0, .capacity=vectors_min, .type=type, .data=data};
+}
+
+bool vectors_push(vector *v, void *item) {
+	errors_panic("vectors_push (v veio nulo)", v == NULL);
+	errors_warn("vectors_push (item veio 0)", item == 0);
+
+	v->size++;
+	if (v->size > v->capacity - 1) {
+		v->capacity = v->capacity << 1;
+
+		size_t type_size = vectors_types_size(v->type);
+		void *new_data = realloc(v->data, (v->capacity)*type_size);
+
+		if (new_data == NULL) {
+			printf(colors_warn("vectors_push (new_data == NULL)"));
+			return true; 
+		}
+
+		v->data = new_data;
+	}
+
+	vectors_types_insert(v->type, v->data, item, v->size - 1);
+
+	return false;
+}
+
+bool vectors_pop(vector *v) {
+	errors_panic("vectors_pop (v veio nulo)", v == NULL);
+
+	if (v->size == 0) {
+		printf(colors_warn("vectors_pop (v.size veio 0)"));
+		return true;
+	}
+
+	vectors_types_cleanup(v->type, v->data, v->size - 1);
+	v->size--;
+
+	if (v->size < (v->capacity >> 1)) {
+		v->capacity = v->capacity >> 1;
+
+		size_t type_size = vectors_types_size(v->type);
+		void *new_v = realloc(v->data, v->capacity*type_size);
+
+		if (new_v == NULL) {
+			printf(colors_warn("vectors_pop (new_v veio nulo)"));
+			return true; 
+		}
+
+		v->data = new_v;
+	}
+
+	return false; 
+}
+
+
+//void vectors_sort(vector *v, compfunc compare) {
+//	errors_panic("vectors_sort (v veio nulo)", v == NULL);
+//
+//	if (v->size <= 1) {
+//		printf(color_warn("vectors_sort (v.size <= 1)"));
+//		return;
+//	}
+//
+//	for (size_t i = v->size; i > 0; i--) {
+//		for (size_t j = 1; j < i; j++) {
+//
+//			void *prev_data = v->data + j - 1;
+//			void *curr_data = v->data + j;
+//
+//			bool is_bigger = compare(prev_data, curr_data);
+//			if (is_bigger) {
+//				vectors_types_insert(v->type, v->data, v->data + j, v->capacity - 1);
+//				vectors_types_insert(v->type, v->data, v->data + j - 1, j);
+//				vectors_types_insert(v->type, v->data, v->data + v->capacity - 1, j - 1);
+//			}
+//		}
+//	}
+//}
+
+void vectors_free(vector *v) {
+	if (v == NULL) {
+		printf(colors_warn("vectors_free (v == NULL)"));
+		return;
+	}
+
+	while (v->size > 0) {
+		vectors_types_cleanup(v->type, v->data, v->size - 1);
+		v->size--;
+	}
+
+	free(v->data);
+	v = NULL;
+}
+
+#endif
---
+++ lib/vectors.h
@@ -0,0 +1,60 @@
+#ifndef vectors_h
+#define vectors_h
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/cdefs.h>
+
+#define vectors_min 16
+
+typedef enum _vector_types {
+	vectors_string_type,
+	vectors_vector_type,
+	vectors_size_type,
+	vectors_string_virtual_type,
+} vectors_type;
+
+typedef struct _vector_struct {
+	vectors_type type;
+	size_t size;
+	size_t capacity;
+	void *data;
+} vector;
+
+typedef struct _set_struct {
+	vector hashs; //size_t
+	vector freqs; //size_t
+	vector itens; //void *
+} set;
+
+#include "errors.h"
+#include "strings.h"
+
+typedef bool (*compfunc)(void *, void *);
+typedef size_t (*hashfunc)(void *);
+typedef void (*cleanfunc)(void *);
+
+
+__attribute_warn_unused_result__
+size_t vectors_types_size(vectors_type type);
+
+void vectors_types_insert(vectors_type type, void *data, void *item, size_t index);
+
+void vectors_types_cleanup(vectors_type type, void *data, size_t index);
+
+
+__attribute_warn_unused_result__
+vector vectors_init(vectors_type type);
+
+bool vectors_push(vector *v, void *item);
+
+bool vectors_pop(vector *v);
+
+/*insertion sort*/
+void vectors_sort(vector *v, compfunc compare);
+
+void vectors_free(vector *v);
+
+#endif
