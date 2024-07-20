5f58e8dac65b1a58aeafc48340e5454dfbdd4d2b
Angelus Rocha <angelusrt@gmail.com>
2024-07-20 17:48:14 -0300

    adicionando biblioteca de strings

---
+++ lib/strings.c
@@ -0,0 +1,378 @@
+#ifndef strings_c
+#define strings_c
+#pragma once
+
+#include "strings.h"
+#include "vectors.c"
+#include "errors.c"
+
+
+string strings_init(const strings_size size) {
+	char *text = calloc(size, sizeof(char));
+    errors_panic("strings_init (text)", text == NULL);
+
+    return (string) {.size=1, .capacity=size, .text=text};
+}
+
+string strings_make(const char *lit) {
+    errors_panic("strings_make (lit)", lit == NULL);
+    errors_warn("strings_make lit == '\\0'", strncmp(lit, "\0", 1) == 0);
+
+	size_t new_size = strlen(lit) + 1;
+	size_t new_capacity = strings_min;
+	while (new_capacity < new_size) {
+		new_capacity = new_capacity << 1;
+	}
+
+	string new_string = strings_init(new_capacity);
+    new_string.size = new_size;
+
+    strncpy(new_string.text, lit, new_string.capacity);
+    new_string.text[new_string.size - 1] = '\0';
+    new_string.text[new_string.capacity - 1] = '\0';
+
+    return new_string;
+}
+
+bool strings_upgrade(string *s) {
+	errors_panic("strings_upgrade (s)", s);
+	errors_panic("strings_upgrade (s.size == 0)", s->size == 0);
+	errors_panic("strings_upgrade (s.capacity < strings_min)", s->capacity < strings_min);
+
+	bool less_than_half_capacity = s->size - 1 < (s->capacity >> 1);
+	bool bigger_than_min = s->size - 1 > strings_min;
+	bool almost_big_as_capacity = s->size >= s->capacity - 1;
+	bool almost_big_as_max = s->capacity >= (SIZE_MAX >> 1);
+
+	if (almost_big_as_max) {
+		return true;
+	}
+
+	size_t new_capacity = 0;
+	if (almost_big_as_capacity) {
+		new_capacity = s->capacity << 1;
+	} else if (less_than_half_capacity && bigger_than_min) {
+		new_capacity = s->capacity >> 1;
+	}
+
+	if (almost_big_as_capacity || (less_than_half_capacity && bigger_than_min)) {
+		char *new_text = realloc(s->text, new_capacity*sizeof(char));
+		errors_warn("realloc falhou - prosseguindo sem.", new_text == NULL);
+
+		if (new_text == NULL) { return true; }
+
+		s->text = new_text;
+		s->capacity = new_capacity;
+		s->text[s->capacity - 1] = '\0';
+	}
+
+	return false;
+}
+
+void strings_free(string *s) {
+	if (s == NULL) { 
+		printf(colors_warn("strings_free (s == NULL)"));
+		return; 
+	}
+
+    free(s->text);
+    s->text = NULL;
+    //free(s);
+    s = NULL;
+}
+
+void strings_free_many(size_t n, string *s, ...) {
+    errors_panic("strings_free (n == 0)", n == 0);
+    errors_panic("strings_free (s == 0)", s == 0);
+
+    va_list args;
+    va_start(args, s);
+	strings_free(s);
+
+    for (size_t i = 0; i < (size_t)(n - 1); i++) {
+        string *arg_s = va_arg(args, string *);
+
+		if (arg_s == NULL) {
+			printf(colors_warn("strings_free (arg_s == NULL)"));
+			continue;
+		}
+
+		strings_free(arg_s);
+    }
+
+    va_end(args);
+}
+
+vector strings_find(const string *s, const string *sep) {
+    errors_panic("strings_find (s)", s == NULL);
+    errors_panic("strings_find (sep)", sep == NULL);
+    errors_panic("strings_find (s.size <= 1)", s->size <= 1);
+    errors_panic("strings_find (sep.size <= 1)", sep->size <= 1);
+
+	vector indexes = vectors_init(sizeof(size_t));
+
+    for (size_t i = 0, j = 0; i < s->size - 1; i++) {
+        if (s->text[i] == sep->text[0]) {
+            j = 1;
+        } else if (s->text[i] == sep->text[j]) {
+            j++;
+		} else {
+            j = 0;
+        }
+
+        if (j == sep->size - 1) {
+			vectors_push(&indexes, (void *)(i - (j - 1)));
+        }
+    }
+
+    return indexes;
+}
+
+void strings_replace(string *s, const string *seps, const char rep[1]) {
+    errors_panic("strings_replace (s)", s == NULL);
+    errors_panic("strings_replace (seps)", seps == NULL);
+    errors_panic("strings_replace (s <= 1)", s->size <= 1 );
+    errors_panic("strings_replace (sep <= 1)", seps->size <= 1);
+
+    for (size_t i = 0; i < s->size - 1; i++) {
+		for (size_t j = 0; j < seps->size - 1; j++) {
+			if (s->text[i] == seps->text[j]) {
+				s->text[i] = rep[0];
+				break;
+			} 
+		}
+    }
+}
+
+string strings_make_replace(const string *s, const string *sep, const string *rep) {
+    errors_panic("strings_make_replace (s)", s == NULL);
+    errors_panic("strings_make_replace (sep)", sep == NULL);
+    errors_panic("strings_make_replace (rep)", rep == NULL);
+    errors_panic("strings_make_replace (s.size <= 1)", s->size <= 1);
+    errors_panic("strings_make_replace (sep.size <= 1)", sep->size <= 1);
+    errors_panic("strings_make_replace (rep.size == 0)", rep->size == 0);
+
+    long diff_size = (rep->size - sep->size);
+    size_t size = s->size + (sep->size - 1) * diff_size;
+
+	vector indexes = strings_find(s, sep);
+	if (indexes.size == 0) {
+		vectors_free(&indexes);
+		return strings_init(strings_min);
+	}
+
+	size_t capacity = strings_min;
+	while (capacity < size) {
+		capacity = capacity << 1;
+	}
+
+	string new_string = strings_init(capacity);
+    new_string.size = size;
+
+    size_t prev = 0, i = 0, j = 0;
+    while (i < s->size - 1) {
+		size_t index = ((size_t *)indexes.data)[j];
+		size_t offset = i + j * diff_size;
+		size_t sentence_size = 0;
+
+        if (j < indexes.size && index == i) {
+            strncpy(new_string.text + offset, rep->text, rep->size);
+            prev = index + sep->size - 1;
+            i += sep->size - 1;
+            j += 1;
+        } else {
+            if (j > indexes.size - 1) {
+                sentence_size = s->size - prev;
+            } else {
+                sentence_size = index - prev;
+            }
+
+            strncpy(new_string.text + offset, s->text + prev, sentence_size);
+            i += sentence_size;
+        }
+    }
+
+	vectors_free(&indexes);
+    return new_string;
+}
+
+vector strings_trim(const string *s, const string *sep) {
+    errors_panic("strings_trim (s)", s == NULL);
+    errors_panic("strings_trim (sep)", sep == NULL);
+    errors_panic("strings_trim (s.size <= 1)", s->size <= 1);
+    errors_panic("strings_trim (sep.size <= 1)", sep->size <= 1);
+
+	vector indexes = strings_find(s, sep);
+    vector sentences = vectors_init(vectors_string_type);
+
+	if (indexes.size == 0) {
+		vectors_free(&indexes);
+		printf(colors_warn("strings_trim (indexes.size == 0)"));
+		return sentences;
+	}
+
+    size_t prev = 0, size = 0;
+    for (size_t i = 0; i < indexes.size; i++) {
+		size_t index = ((size_t *)indexes.data)[i];
+
+        if (i > indexes.size - 1) {
+            size = s->size - prev + 1;
+        } else {
+            size = index - prev + 1;
+        }
+
+        if (size <= 1) {
+            prev = index + (sep->size - 1);
+            continue;
+        }
+
+        if (s->text[prev] == '\0') {
+            break;
+        }
+
+		size_t capacity = strings_min;
+		while (capacity < size) {
+			capacity = capacity << 1;
+		}
+
+		string new_string = strings_init(capacity);
+        new_string.size = size;
+
+        strncpy(new_string.text, s->text + prev, size - 1);
+        new_string.text[size - 1] = '\0';
+
+        vectors_push(&sentences, &new_string);
+
+        if (i > indexes.size - 1) {
+            break;
+        }
+
+        //" Ä" -> {194, 160, 195, 132}
+        // if (i == 34) { printf("%zu %zu %zu\n", prev, sf->indexes[i],
+        // sf->indexes[i+1]); }
+
+        prev = index + (sep->size - 1);
+    }
+
+	vectors_free(&indexes);
+    return sentences;
+}
+
+void strings_trim_virtual(const string *s, const string *sep, vector *sentences) {
+    errors_panic("strings_trim_virtual (s)", s == NULL);
+    errors_panic("strings_trim_virtual (sep)", sep == NULL);
+    errors_panic("strings_trim_virtual (s.size <= 1)", s->size <= 1);
+    errors_panic("strings_trim_virtual (sep.size <= 1)", sep->size <= 1);
+
+	vector indexes = strings_find(s, sep);
+	if (indexes.size == 0) {
+		printf(colors_warn("strings_trim_virtual (indexes.size == 0)"));
+		vectors_free(&indexes);
+		return;
+	}
+
+	string_virtual new_string_virtual;
+    size_t prev = 0, size = 0;
+    for (size_t i = 0; i < indexes.size; i++) {
+		size_t index = ((size_t *)indexes.data)[i];
+
+        if (i > indexes.size - 1) {
+            size = s->size - prev + 1;
+        } else {
+            size = index - prev + 1;
+        }
+
+        if (size <= 1) {
+            prev = index + (sep->size - 1);
+            continue;
+        }
+
+        if (s->text[prev] == '\0') {
+            break;
+        }
+
+		if (sentences->size <= i) {
+			new_string_virtual.size = size; 
+			new_string_virtual.text = s->text + prev;
+			vectors_push(sentences, &new_string_virtual);
+		} else {
+			string_virtual *old_string_virtual = (string_virtual *)sentences->data+i;
+			old_string_virtual->size = size;
+			old_string_virtual->text = s->text + prev;
+		}
+
+        //if (i > indexes.size - 1) {
+        //    break;
+        //}
+
+        prev = index + (sep->size - 1);
+    }
+
+	vectors_free(&indexes);
+}
+
+set strings_trim_unique(const string *s) {
+	vector hashs = vectors_init(vectors_size_type);
+	vector freqs = vectors_init(vectors_size_type);
+	vector tokens = vectors_init(vectors_string_type);
+
+	string token = strings_make(s->text);
+	size_t hash = strings_hasherize(&token);
+
+	vectors_push(&hashs, (void *)hash);
+	vectors_push(&freqs, (void *)1);
+	vectors_push(&tokens, &token);
+
+	for (size_t i = 0; i < s->size - 1; i++) {
+		if (s->text[i] == '\0' && s->text[i + 1] != '\0') {
+			token = strings_make(s->text+i+1);
+			hash = strings_hasherize(&token);
+
+			bool found = false;
+			for (size_t j = 0; j < hashs.size - 1; j++) {
+				if (((size_t *)hashs.data)[j] == hash) {
+					((size_t *)freqs.data)[j] += 1;
+					found = true;
+					break;
+				}
+			}
+
+			if (found == false) {
+				vectors_push(&hashs, (void *)hash);
+				vectors_push(&freqs, (void *)1);
+				vectors_push(&tokens, &token);
+			} else {
+				strings_free(&token);
+			}
+		} 
+	}
+
+	return (set){.hashs=hashs, .itens=tokens, .freqs=freqs};
+}
+
+size_t strings_hasherize(const string *s) {
+    errors_panic("strings_hasherize (s)", s == NULL);
+    size_t hash = 0;
+
+    for (size_t i = 0; i < s->size - 1; i++) {
+        unsigned char character = s->text[i];
+        size_t power = pow(hash_const, i + 1);
+        hash += character * power;
+    }
+
+    return hash;
+}
+
+void strings_print(const string *s) {
+    errors_panic("strings_print (s)", s == NULL);
+    errors_panic("strings_print (s.size <= 1)", s->size <= 1);
+    errors_warn("strings_print (s.size < 2)", s->size < 2);
+
+    for (size_t i = 0; i < s->size - 1; i++) {
+        printf("%c", s->text[i]);
+    }
+
+    printf("\n");
+}
+
+#endif
---
+++ lib/strings.h
@@ -0,0 +1,77 @@
+#ifndef strings_h
+#define strings_h
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <math.h>
+
+#include "errors.h"
+#include "vectors.h"
+
+#define hash_const 7
+
+typedef struct _string_virtual_struct {
+	size_t size;
+	char *text;
+} string_virtual;
+
+typedef struct _string_struct {
+	size_t size;
+	char *text;
+	size_t capacity;
+} string;
+
+typedef enum _strings_size {
+	strings_min = 16,
+	strings_small = 1024,
+	strings_big = 4096,
+	strings_large = 65536,
+	strings_very_large = 1048576, 
+	strings_max = SIZE_MAX,
+} strings_size;
+
+__attribute_warn_unused_result__
+string strings_init(strings_size size);
+
+__attribute_warn_unused_result__
+string strings_make(const char *lit);
+
+//returns if it failed to resize
+__attribute_warn_unused_result__
+bool strings_upgrade(string *s);
+
+void strings_free(string *s);
+
+void strings_free_many(size_t n, string *s, ...);
+
+//returns vector<size_t>
+__attribute_warn_unused_result__
+vector strings_find(const string *s, const string *sep);
+
+void strings_replace(string *s, const string *seps, const char rep[1]);
+
+__attribute_warn_unused_result__
+string strings_make_replace(const string *s, const string *sep, const string *rep);
+
+__attribute_warn_unused_result__
+vector strings_trim(const string *s, const string *sep);
+
+__attribute_warn_unused_result__
+set strings_trim_unique(const string *s);
+
+///__attribute_warn_unused_result__
+//vector strings_trim_virtual(string *s, string *sep);
+
+void strings_trim_virtual(const string *s, const string *sep, vector *sentences);
+
+__attribute_warn_unused_result__
+size_t strings_hasherize(const string *s);
+
+void strings_print(const string *s);
+
+
+#endif
